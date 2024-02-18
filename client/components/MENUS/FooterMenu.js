700140f7ada214c3596fa6a0e7a2c5181e3b0c51
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-02-18 22:53:13 +0530

    Footer Menu

--- client/components/MENUS/FooterMenu.js
+++ client/components/MENUS/FooterMenu.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import Home from "../../screens/Home";
 import Post from "../../screens/Post";
@@ -9,22 +9,39 @@ import Account from "../../screens/Account";
 
 const FooterMenu = () => {
   const navigation = useNavigation();
+  const route = useRoute();
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={()=> navigation.navigate('Home')}>
-        <FontAwesome5 name="home" style={styles.iconstyle} />
+      <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+        <FontAwesome5
+          name="home"
+          style={styles.iconstyle}
+          color={route.name === "Home" && "orange"}
+        />
         <Text>Home</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={()=> navigation.navigate('Post')}>
-        <FontAwesome5 name="plus-square" style={styles.iconstyle} />
+      <TouchableOpacity onPress={() => navigation.navigate("Post")}>
+        <FontAwesome5
+          name="plus-square"
+          style={styles.iconstyle}
+          color={route.name === "Post" && "orange"}
+        />
         <Text>Post</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={()=> navigation.navigate('About')}>
-        <FontAwesome5 name="info-circle" style={styles.iconstyle} />
+      <TouchableOpacity onPress={() => navigation.navigate("About")}>
+        <FontAwesome5
+          name="info-circle"
+          style={styles.iconstyle}
+          color={route.name === "About" && "orange"}
+        />
         <Text>About</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={()=> navigation.navigate('Account')}>
-        <FontAwesome5 name="user" style={styles.iconstyle} />
+      <TouchableOpacity onPress={() => navigation.navigate("Account")}>
+        <FontAwesome5
+          name="user"
+          style={styles.iconstyle}
+          color={route.name === "Account" && "orange"}
+        />
         <Text>Account</Text>
       </TouchableOpacity>
     </View>
