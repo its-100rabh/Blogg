ed15f3d6366a383735ab894a428e996d9e6a66ba
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-02-20 10:24:15 +0530

    Update Profile UI

--- client/screens/Account.js
+++ client/screens/Account.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React, { useContext } from "react";
 import { AuthContext } from "../context/authContext";
 import FooterMenu from "../components/MENUS/FooterMenu";
@@ -7,6 +7,17 @@ const Account = () => {
   const [state] = useContext(AuthContext);
   return (
     <View style={styles.container}>
+      <View style={{ alignItems: "center" }}>
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+          }}
+          style={{ height: 200, width: 200 }}
+        />
+      </View>
+      <Text style={styles.warningtext}>
+        You can currently update your name and password only.
+      </Text>
       <Text>NAME : {state?.user.name}</Text>
       <Text>Email : {state?.user.email}</Text>
       <Text>ROLE : {state?.user.role}</Text>
@@ -24,6 +35,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-between",
     marginTop: 40,
+    margin: 10,
+  },
+  warningtext: {
+    color: "red",
+    fontSize: 12,
+    textAlign: "center",
     margin: 10,
   },
 });
