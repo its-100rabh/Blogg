9d9df8f97935a9033eda69f70b574e2c7059867b
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-02-21 01:32:57 +0530

    Update Function

--- client/screens/Account.js
+++ client/screens/Account.js
@@ -1,50 +1,99 @@
-import { Image, StyleSheet, Text, TextInput, View } from "react-native";
-import React, { useContext } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  ScrollView,
+} from "react-native";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../context/authContext";
 import FooterMenu from "../components/MENUS/FooterMenu";
+import axios from "axios";
 
 const Account = () => {
-  const [state] = useContext(AuthContext);
+  const [state, setState] = useContext(AuthContext);
+  const { user } = state;
+  //local state
+  const [name, setName] = useState(user?.name);
+  const [password, setPassword] = useState(user?.password);
+  const [email] = useState(user?.email);
+  const [loading, setLoading] = useState(false);
+
+  //onPress
+  const handleUpdate = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.put("/auth/update-user", {
+        name,
+        password,
+        email,
+      });
+      setLoading(false);
+      let ud = JSON.stringify(data);
+      setState({ ...state, user: ud?.updatedUser });
+      alert(data && data.message);
+    } catch (error) {
+      alert(error.response.data.message);
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <View style={{ alignItems: "center" }}>
-        <Image
-          source={{
-            uri: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-          }}
-          style={{ height: 200, width: 200 }}
-        />
-      </View>
-      <Text style={styles.warningtext}>
-        You can currently update your name and password only.
-      </Text>
-      <View style={styles.input}>
-        <Text style={styles.inputText}>Name</Text>
-        <TextInput style={styles.inputBox} value={state?.user.name} />
-      </View>
-      <View style={styles.input}>
-        <Text style={styles.inputText}>E-mail</Text>
-        <TextInput
-          style={styles.inputBox}
-          value={state?.user.email}
-          editable={false}
-        />
-      </View>
-      <View style={styles.input}>
-        <Text style={styles.inputText}>Password</Text>
-        <TextInput style={styles.inputBox} value={state?.user.password} />
-      </View>
-      <View style={styles.input}>
-        <Text style={styles.inputText}>Role</Text>
-        <TextInput
-          style={styles.inputBox}
-          value={state?.user.role}
-          editable={false}
-        />
-      </View>
-      {/* <Text>NAME : {state?.user.name}</Text>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+            }}
+            style={{ height: 200, width: 200 }}
+          />
+        </View>
+        <Text style={styles.warningtext}>
+          You can currently update your name and password only.
+        </Text>
+        <View style={styles.input}>
+          <Text style={styles.inputText}>Name</Text>
+          <TextInput
+            style={styles.inputBox}
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+        </View>
+        <View style={styles.input}>
+          <Text style={styles.inputText}>E-mail</Text>
+          <TextInput style={styles.inputBox} value={email} editable={false} />
+        </View>
+        <View style={styles.input}>
+          <Text style={styles.inputText}>Password</Text>
+          <TextInput
+            style={styles.inputBox}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
+          />
+        </View>
+        <View style={styles.input}>
+          <Text style={styles.inputText}>Role</Text>
+          <TextInput
+            style={styles.inputBox}
+            value={state?.user.role}
+            editable={false}
+          />
+        </View>
+        <View style={{ alignItems: "center", margin: 20 }}>
+          <TouchableOpacity style={styles.updateBtn} onPress={handleUpdate}>
+            <Text style={styles.updateBtntext}>Update Profile</Text>
+          </TouchableOpacity>
+        </View>
+        {/* <Text>NAME : {state?.user.name}</Text>
       <Text>Email : {state?.user.email}</Text>
+      
       <Text>ROLE : {state?.user.role}</Text> */}
+      </ScrollView>
       <View style={{ flex: 1, justifyContent: "flex-end" }}>
         <FooterMenu />
       </View>
@@ -86,5 +135,17 @@ const styles = StyleSheet.create({
     fontSize: 16,
     paddingLeft: 20,
     borderRadius: 5,
+  },
+  updateBtn: {
+    backgroundColor: "black",
+    height: 40,
+    width: 250,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  updateBtntext: {
+    color: "white",
+    fontSize: 16,
   },
 });
