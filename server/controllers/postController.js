8b7aaac929a80e9125183953ac60be8215e33e83
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-02-24 10:33:52 +0530

    Posts API completed

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,3 +1,33 @@
-const createPostController = () => {};
+const postModel = require("../models/postModel");
+
+const createPostController = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    if (!title || !description) {
+      return res.status(500).send({
+        success: false,
+        message: "Please provide all Fields",
+      });
+    }
+    const post = await postModel({
+      title,
+      description,
+      postedBy: req.auth._id,
+    }).save();
+    res.status(201).send({
+        success:true,
+        message:'Posts Created Successfully',
+        post
+    })
+    console.log(req);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: true,
+      message: "Error in Create Post API",
+      error,
+    });
+  }
+};
 
 module.exports = { createPostController };
