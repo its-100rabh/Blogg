b162b43093772025f3b516b2c7dfc4f20413c22b
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-02-25 22:26:46 +0530

    Post Screen

--- client/screens/Post.js
+++ client/screens/Post.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useContext } from "react";
 import { AuthContext } from "../context/authContext";
 import FooterMenu from "../components/MENUS/FooterMenu";
@@ -6,6 +6,21 @@ import FooterMenu from "../components/MENUS/FooterMenu";
 const Post = () => {
   return (
     <View style={styles.container}>
+      <View style={{ alignItems: "center" }}>
+        <Text style={styles.heading}>Create a Post</Text>
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Add post title"
+          placeholderTextColor={"gray"}
+        />
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Add post description"
+          placeholderTextColor={"gray"}
+          multiline={true}
+          numberOfLines={6}
+        />
+      </View>
       <View style={{ flex: 1, justifyContent: "flex-end" }}>
         <FooterMenu />
       </View>
@@ -19,7 +34,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "space-between",
-    marginTop: 40,
     margin: 10,
+    marginTop: 20,
+  },
+  heading: {
+    textTransform: "uppercase",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  inputBox: {
+    backgroundColor: "white",
+    width: 320,
+    marginTop: 30,
+    fontSize: 13,
+    paddingLeft: 15,
+    borderWidth: 1,
   },
 });
