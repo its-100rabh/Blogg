a6e60b98d0ebb5180ac84a90d72a737edec357af
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-02-26 20:10:52 +0530

    Post Context

--- client/navigation.js
+++ client/navigation.js
@@ -2,11 +2,14 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { AuthProvider } from "./context/authContext";
 import ScreenMenu from "./components/MENUS/ScreenMenu";
+import { PostProvider } from "./context/postContext";
 
 const RootNavigation = () => {
   return (
     <AuthProvider>
-      <ScreenMenu />
+      <PostProvider>
+        <ScreenMenu />
+      </PostProvider>
     </AuthProvider>
   );
 };
